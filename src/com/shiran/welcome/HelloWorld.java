4a71dcdb5f2af45d68fc8526fcf7e3ce1b6c079f
Shiran Iroshan <shiraniroshan@gmail.com>
2019-03-27 12:15:00 +0530

    Add new method.

--- src/com/shiran/welcome/HelloWorld.java
+++ src/com/shiran/welcome/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    private void showMe() {
+        System.out.println("show me");
+    }
 }
