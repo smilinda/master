a772dbf65224e4dd01a5cd0349be4169f6b32b03
Shiran Iroshan <shiraniroshan@gmail.com>
2019-03-27 12:11:30 +0530

    Add new print lines1.

--- src/com/shiran/welcome/HelloWorld.java
+++ src/com/shiran/welcome/HelloWorld.java
@@ -3,5 +3,9 @@ package com.shiran.welcome;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
